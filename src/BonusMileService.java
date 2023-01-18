06427aef3e1ca15338094c3700f67caf1b9bc03e
Viacheslav Kolarzh <guin-s@yandex.ru>
2023-01-18 12:13:48 +0300

    HW3-1

---
+++ src/BonusMileService.java
@@ -0,0 +1,10 @@
+public class BonusMileService {
+    int bonus;
+    public int calculate (int price){
+        bonus =price/20;
+        return bonus;
+    }
+
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price = 100; // цена билета
-        int divider = 20; // кратность начисления бонусных миль
-        int bonus = price/divider; // количество начисленных бонусных миль
-
-        System.out.println("За покупку билета будет начислено " +bonus +" бонусных миль");
+        BonusMileService service = new BonusMileService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
